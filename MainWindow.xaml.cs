f2175fdd10138694d574daa61ae1287f782e51bf
theOne695 <115659409+theOne695@users.noreply.github.com>
2023-05-17 23:23:00 +0300

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.WebRequestMethods;
 
 namespace pract3
 {
@@ -24,5 +27,29 @@ namespace pract3
         {
             InitializeComponent();
         }
+
+        private void OpenBtn_Click_1(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog { IsFolderPicker = true };
+            var result = dialog.ShowDialog();
+
+            if (result == CommonFileDialogResult.Ok)
+            {
+                string[] files = Directory.GetFiles(dialog.FileName);
+                FileTxt.ItemsSource = files;
+            }
+        }
+
+        private void audioSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            media.Position = new TimeSpan(Convert.ToInt64(audioSlider.Value));
+        }
+
+        private void media_MediaOpened_1(object sender, RoutedEventArgs e)
+        {
+            audioSlider.Maximum = media.NaturalDuration.TimeSpan.Ticks;
+        }
+
+        
     }
 }
